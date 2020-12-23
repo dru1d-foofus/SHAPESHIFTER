4d91d24200fbf4ca07ecc392f7cb9d82ef548b04
Tyler <4245930+dru1d-foofus@users.noreply.github.com>
2020-12-23 01:17:42 -0500

    removing unused imports from testing

--- SHAPESHIFTER/Templates/Stage0Template.cs
+++ SHAPESHIFTER/Templates/Stage0Template.cs
@@ -6,8 +6,6 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Reflection;
-using System.Text;
-using System.Text.RegularExpressions;
 
 namespace Stage0
 {
